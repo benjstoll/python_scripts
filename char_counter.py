b6da683ccd1455a0ae8f51d9cc2703f0ce83c25b
Ben J Stoll <31862934+benjstoll@users.noreply.github.com>
2020-08-14 08:46:47 -0500

    Update char_counter.py
    added comments

--- char_counter.py
+++ char_counter.py
@@ -1,12 +1,15 @@
 sentence = "what is the most used character in this sentence?"
 
 char_frequency = {}
+
+#checks to see if character is in list, if not, add to list.
 for char in sentence:
     if char in char_frequency:
         char_frequency[char] += 1
     else:
         char_frequency[char] = 1
 
+#sort tuple in ascending order 
 sorted_char_frequency = sorted(char_frequency.items(), key=lambda item:item[1])
 
 print(sorted_char_frequency)
